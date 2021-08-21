0af36b70ee8c6f196b63c426dded1c9fafee8763
PragmaticCreator <aaronchristopher.developer@gmail.com>
2021-08-21 22:08:28 +0530

    set up file structure

---
+++ src/root/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import Blog from 'views/Blog';
+
+function App() {
+  return (
+    <div className='App'>
+      <Blog />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/Blog.js
@@ -0,0 +1,9 @@
+const Blog = () => {
+  return (
+    <div>
+      <h1>Hey there bro</h1>
+    </div>
+  );
+};
+
+export default Blog;
